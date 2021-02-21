72b72ba4e4ecea8675a457b6281cbfce01b6a50c
sanchi <sanchiagarwal231@gmail.com>
2021-02-21 14:08:32 +0530

    added search changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,3 +9,18 @@ function closeNav()
 {
     document.getElementById("mySidebar").style.width = "0";
 }
+
+// Connecting google search to the products page
+const f = document.getElementById('searchform');
+const q = document.getElementById('q');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = '../products.html'
+
+function submitted(event){
+    event.preventDefault();
+    const url = google + site + '+' + q.value;
+    const win = window.open(url, '_blank');
+    win.focus();
+}
+
+f.addEventListener('submit', submitted);
